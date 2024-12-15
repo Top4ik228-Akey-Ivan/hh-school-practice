e6def6ac31c0e989ab474355001fa916d07fc0f0
Ivan Orekhov <ivanorekhov04022005@gmail.com>
2024-12-15 11:51:06 +0300

    js jsx files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    '*': ['yarn format --', 'yarn lint:fix'],
+    '* .js': ['yarn format --', 'yarn lint:fix'],
+    '* .html': ['yarn format --', 'yarn lint:fix'],
 };
