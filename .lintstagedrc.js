e138467178d53422a68ff8fb3339568d3fea5a3a
Ivan Orekhov <ivanorekhov04022005@gmail.com>
2024-12-17 09:41:55 +0300

    убрал Lint-stage из package.json

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    '* .js': ['yarn format --', 'yarn lint:fix'],
-    '* .html': ['yarn format --', 'yarn lint:fix'],
+    '*.js': ['yarn format --', 'yarn lint:fix'],
+    '*.html': ['yarn format --', 'yarn lint:fix'],
 };
