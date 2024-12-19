a092871fea9734a683a87a32c53691ff63f5868d
Ivan Orekhov <ivanorekhov04022005@gmail.com>
2024-12-20 01:12:49 +0300

    Добавил eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,13 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    {
-        languageOptions: {
-            globals: globals.browser,
-        },
-        rules: {
-            'space-infix-ops': 'error', // Добавлено правило для пробелов вокруг операторов
-        },
+  {
+    languageOptions: {
+      globals: globals.browser,
     },
-    pluginJs.configs.recommended,
+    rules: {
+      'space-infix-ops': 'error', // Добавлено правило для пробелов вокруг операторов
+    },
+  },
+  pluginJs.configs.recommended,
 ];
